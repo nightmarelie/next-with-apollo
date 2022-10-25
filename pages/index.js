b50b6e4b4baad1669b8814befebec8f277d651ca
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-10-24 20:06:38 -0700

    feat: get static props

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
 
 export default function Home() {
   return (
@@ -17,7 +19,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +60,32 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+  };
 }
